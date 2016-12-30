333e28c2f6c7b442d856b04975482c4573268168
Cristopher Guerrero <axl_699@hotmail.com>
2016-12-30 12:55:49 -0600

    0.2
    Se agregó título a la página, tipo de lenguaje, soporte para HTML5 y
    soporte de caracteres UTF-8.

--- test.php
+++ test.php
@@ -1,7 +1,11 @@
-<HTML>
-<HEAD>
-</HEAD>
-<BODY>
-<p>Esto es un comentario inicial</p>
-</BODY>
+<!DOCTYPE HTML>
+<HTML lang="es">
+	<HEAD>
+		<title>Página de prueba</title>
+		<meta charset="utf-8"/>
+	</HEAD>
+	<BODY>
+		<p>Esto es un comentario inicial.</p>
+		<p>Modificaciones en el tipo de lenguaje, compatibilidad de caracteres y título de la página.</p>
+	</BODY>
 </HTML>
